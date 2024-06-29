ab13551aa068ba8be1d013bffec612059b978767
Rishab-Rawat <rawatrishab66@gmail.com>
2024-06-29 16:26:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+describe('Modal Test', () => {
+  it('Opens and closes the Bootstrap modal', () => {
+    cy.visit('http://localhost:3000/main.html');
+
+    // Click the button to open the modal
+    cy.get('#open-modal').click();
+
+    // Assert that the modal is visible
+    cy.get('#newton-modal').should('have.class', 'show');
+
+    // Click the close button in the modal footer
+    cy.get('.modal-footer button').click();
+
+    // Assert that the modal is closed
+    cy.get('#newton-modal').should('not.have.class', 'show');
+  });
+});
